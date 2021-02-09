c79f251bbe63ce00fcb84c23461d08f906266b14
Steven Stier <stiersl@stier-automation.com>
2021-02-09 09:24:41 -0500

    removed weatherforecast

--- src/main/java/com/pg/plantapps/nextactivitiesappservice/model/dao/WeatherForecastDAO.java
+++
@@ -1,9 +0,0 @@
-package com.pg.plantapps.nextactivitiesappservice.model.dao;
-
-import java.util.List;
-import com.pg.plantapps.nextactivitiesappservice.model.WeatherForecast;
-
-public interface WeatherForecastDAO {
-
-	public List<WeatherForecast> getWeatherForcast();
-}
